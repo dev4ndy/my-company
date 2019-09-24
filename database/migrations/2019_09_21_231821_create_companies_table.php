e5cd89cb7f22c95769ada0bb189fa90e1e23d7b6
Andres David Echeverri Jimenez <andrex.125d@gmail.com>
2019-09-24 12:43:26 -0500

    Fix problem with email length

--- database/migrations/2019_09_21_231821_create_companies_table.php
+++ database/migrations/2019_09_21_231821_create_companies_table.php
@@ -16,7 +16,7 @@ class CreateCompaniesTable extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email', 254)->unique()->nullable();
+            $table->string('email')->unique()->nullable();
             $table->string('website')->unique()->nullable();
             $table->string('logo')->nullable();
             $table->timestamps();
